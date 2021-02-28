6c17cc7526fa5d9a0960a8f4b250e78f0c516840
lanandra <luthfi.anandra@gmail.com>
2021-02-28 22:50:02 +0700

    Edit snapshot file name

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ datenow_formatted = datenow.strftime("%A, %d-%m-%Y %H:%M:%S %Z")
 
 # sudo as root and run etcd backup
 sudo_user = "root"
-etcd_backup = "ETCDCTL_API=3 etcdctl --endpoints https://127.0.0.1:2379 snapshot save snapshot_k8s_ludes.db --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/healthcheck-client.crt --key /etc/kubernetes/pki/etcd/healthcheck-client.key"
+etcd_backup = "ETCDCTL_API=3 etcdctl --endpoints https://127.0.0.1:2379 snapshot save snapshot.db --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/healthcheck-client.crt --key /etc/kubernetes/pki/etcd/healthcheck-client.key"
 run_as_sudo(sudo_user, etcd_backup)
 
 # inform backup has been done
